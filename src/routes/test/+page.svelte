2b10b223e0292f4253bdc74bf00c1edd32045b13
Rick Rodriguez <rickr@debian-BULLSEYE-live-builder-AMD64>
2023-01-24 23:35:13 -0500

    adding test folders, updated +layout.svelte deviceWidth

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { Layout } from "$lib/stores/LayoutStore";
+    import Card from '$lib/test/Card.svelte';
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+
+    let text: string = "Hello";
+
+    // similar to useEffect()
+    $: {
+        console.log($Layout.scrollDirection, text)
+        const data = 3
+        text = text.toUpperCase()
+        setTimeout(() => 1000)
+    }
+
+    
+</script>
+
+<!-- <Card title="My second title"/> -->
+
+<input type="text" bind:value={text} />
+<p>{text}</p>
